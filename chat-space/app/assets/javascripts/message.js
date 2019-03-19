c70836b9e456df789abdd5ab5a9234165c2c2721
subaring <subasuba>
2019-03-19 21:06:04 +0900

    [WIP]Make-AutomaticUpdating
    #WHAT
    チャットの自動更新機能を実装した
    #WHY
    チャット機能を快適に利用できるようにするため

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     message.image ? image = `<img src="${message.image}">` : image = ""
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__upper-info">
                     <div class="message__upper-info__talker">${message.user_name}</div>
                     <div class="message__upper-info__date">${message.created_at}</div>
@@ -12,6 +12,23 @@ $(function(){
                 </div>`
     return html;
   }
+
+  $(function() {
+  function buildMESSAGE(data) {
+    message.image ? image = `<img src="${message.image}">` : image = ""
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">${message.user_name}</div>
+                    <div class="message__upper-info__date">${message.created_at}</div>
+                  </div>
+                  <div class="message__text">${message.content}
+                  </div>
+                  ${image}
+                </div>`
+    return html;
+  }
+
+  })
   $('.textbox').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -38,6 +55,36 @@ $(function(){
       $('.input-box__image').reset();
       $('.input-box__submit-button').attr('disabled', false);
     })
+  })
+  $(function(){
+    $(function() {
+      if(location.href.match(/\/groups\/\d+\/messages/)){
+        setInterval(update, 5000);
+      }
+    });
+    function update() {
+      if($('.message')[0]){
+        var message_id = $('.message:last').data('message-id');
+      } else {
+        var message_id = 0
+      }
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        data: { message: {id: message_id} },
+        dataType: 'json'
+      })
+      .done(function(data){
+        data.forEach(function(data){
+          var html = buildHTML(data)
+          $('.messages').append(html);
+          $('.messages').animate({scrollTop: 10000});
+        })
+      })
+      .fail(function(data){
+        alert('error');
+      })
+    }
   })
 })
 
